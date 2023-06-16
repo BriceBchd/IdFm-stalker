211a1bfed70c8568fd651382fe472b82dc4ba73c
BriceBchd <blanch_b@etna-alternance.net>
2023-06-16 16:34:12 +0200

    🌟feat: ajout lignes de métro + perturbations

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # IdFm-stalker
 Ile de France Mobilité - Récupération des informations de l'API Open Data
+
+## Prérequis
+- Python 3.6 ou supérieur
+- pip
+
+## Installation
+- Cloner le projet
+- Installer les dépendances avec la commande `pip install -r requirements.txt`
+
+## Utilisation
+- Lancer le script `python main.py`
+- Le script va créer un fichier `lines.json` avec la liste des lignes de métro (tram et RER à ajouter plus tard)
+- Le script va créer un fichier `disruptions.json` avec la liste des perturbations sur les lignes de métro (tram et RER à ajouter plus tard)
+
+## TODO
+- [x] Récupérer la liste des lignes de métro
+- [x] Récupérer la liste des perturbations sur les lignes de métro
+- [ ] Ajouter les lignes de tram et RER
+- [ ] Ajouter les perturbations sur les lignes de tram et RER
+- [ ] Ajouter les horaires des prochains passages sur certaines stations
--- main.py
+++ main.py
@@ -7,26 +7,70 @@ from dotenv import load_dotenv
 load_dotenv()
 
 HOST = 'https://prim.iledefrance-mobilites.fr/'
-BASE_PATH = 'marketplace/navitia/line_reports'
+BASE_PATH = 'marketplace/navitia/'
 API_KEY = os.getenv('API_KEY')
 
 # Get all line reports
-def get_line_reports():
-    url = f'{HOST}{BASE_PATH}/coverage/fr-idf/line_reports'
+def get_line_reports(page=0):
+    url = f'{HOST}{BASE_PATH}coverage/fr-idf/lines'
+    params = {
+        'disable_disruption': 'true',
+        'count': 1000,
+        'start_page': page
+    }
     headers = {'apiKey': API_KEY} if API_KEY else None
-    response = requests.get(url, headers=headers)
-    write_to_file(response.json(), 'line_reports.json')
-    return response.json()
+    response = requests.get(url, headers=headers, params=params)
+    print(response.status_code)
+    line_list = []
+    for line in response.json()['lines']:
+        if line['physical_modes'][0]['name'] == 'Métro':
+            # details = {
+            #     'id': line['id'],
+            #     'name': line['name'],
+            #     'code': line['code']
+            # }
+            details = line
+            line_list.append(details)
+    return line_list
 
 # Get all traffic information
-def get_all_traffic_information():
+def get_message_info_traffic():
+    path = 'v1/tr-messages-it'
+    params = {
+        'LineRef': 'ALL'
+    }
+    headers = {'apiKey': API_KEY} if API_KEY else None
+    url = f'{HOST}{path}'
+    response = requests.get(url, params=params, headers=headers)
+    print(response)
+    write_to_file(response.json(), 'message_info_traffic.json')
+
+
+def get_real_time_stop():
     path = 'marketplace/stop-monitoring'
     params = {
         'MonitoringRef': 'STIF:StopPoint:Q:463158:'
     }
     url = f'{HOST}{path}'
-    response = requests.get(url, params=params, headers={'apiKey': API_KEY})
+    headers = {'apiKey': API_KEY} if API_KEY else None
+    response = requests.get(url, params=params, headers=headers)
     write_to_file(response.json(), 'traffic_information.json')
+
+def get_reports_by_line(lines):
+    disruptions = []
+
+    for line in lines:
+        url = f'{HOST}{BASE_PATH}coverage/fr-idf/lines/{line["id"]}/line_reports'
+        headers = {'apiKey': API_KEY} if API_KEY else None
+        response = requests.get(url, headers=headers)
+        print(response.status_code)
+
+        
+        for disruption in response.json()['disruptions']:
+            if disruption['status'] == 'active':
+                disruptions.append(disruption)
+    
+    return disruptions
 
 # # write dict to file
 def write_to_file(data, file_name):
@@ -36,5 +80,10 @@ def write_to_file(data, file_name):
         
 
 if __name__ == '__main__':
-    # get_line_reports()
-    get_all_traffic_information()
+    lines = get_line_reports()
+    lines += get_line_reports(1)
+    write_to_file(lines, 'lines.json')
+    disruptions = get_reports_by_line(lines)
+    print("nb d'incidents: ", len(disruptions))
+    write_to_file(disruptions, 'disruptions.json')
+    # get_message_info_traffic()
